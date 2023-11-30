de897150f6810a574602fd8cf9ede395932dcce2
Aidar <ar.ibraev98@gmail.com>
2023-11-30 21:20:44 +0600

    Add a method to display short path knight took

--- knights.rb
+++ knights.rb
@@ -5,25 +5,51 @@ class Knight
     @position = position
   end
 
-  def valid_moves
+  def valid_moves(position)
     moves = [ 
             [1, 2], [-1, 2], [1, -2], [-1, -2], 
             [2, 1], [-2, 1], [2, -1], [-2, -1] 
             ]
-    moves.select { |move| move[0].between?(1, 8) && move[1].between?(1,8) }
+    possible_moves = moves.map { |move| [move[0] + position[0], move[1] + position[1]] }        
+    possible_moves.select { |move| move[0].between?(1, 8) && move[1].between?(1,8) }
   end
 
   def knight_moves(start, finish)
-    return "Starting and ending positions are the same" if start == finish
-    start = @position
+    return puts "Starting and ending positions are the same" if start == finish
+
     queue = []
     visited = []
     queue.push(start)
     until queue.empty?
-      current_position = queue.shift
+      queue.length.times do
+        current_position = queue.shift
+        visited.push(current_position)
+
+        return display_path(visited, start, finish) if current_position == finish
+
+        valid_moves(current_position).each do |move|
+          if !visited.include?(move) || !queue.include?(move)
+            queue.push(move)
+          end
+        end
+      end
     end
+    return 'No possible moves'
+  end
+
+  def display_path(visited_pos, start_pos, finish_pos)
+    path = []
+    position = finish_pos
+    while position != start_pos
+      path.unshift(position) # better to use unshift instead of push because of the order
+      position = visited_pos.find { |move| valid_moves(move).include?(position) }
+    end
+    path.unshift(start_pos)
+    
+    puts "You made it in #{path.length - 1} moves. Here's your path"
+    path.each { |pos| print "#{pos}\n" }
   end
 end
 
 knight = Knight.new
-knight.knight_moves
+knight.knight_moves([0,0],[3,3])
